08b9c10da902157d9d79c9561dcee0e82ce3eebb
S V Jagadeesh <vjagadeesh083@gmail.com>
2023-10-09 17:10:39 +0530

    update files

--- scripts/docker_container_stop.sh
+++ scripts/docker_container_stop.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+cd /opt
+
+docker build -t javaapp:v1 .
+
+docker images
+
+
+docker run -d --name javaapp-container -p 8080:8080 javaapp:v1
+
+docker ps -a
+
 docker stop javaapp-container
+
 docker rm javaapp-container
