8c8c2b8eea9d0825ccdbe67271ab48ee1f37ec02
S V Jagadeesh <vjagadeesh083@gmail.com>
2023-10-12 11:54:54 +0530

    update appsec file

--- scripts/run_docker_commands.sh
+++ scripts/run_docker_commands.sh
@@ -2,10 +2,21 @@
 
 cd /opt
 
-docker build -t javaapp:latest .
+docker ps -a
+
+doceker stop javaapp-container
+
+docker rm javaapp-container
 
 docker images
 
+docker rmi tomcat
+
+docker rmi javaapp:latest
+
+docker build -t javaapp:latest .
+
+docker images
 
 docker run -d --name javaapp-container -p 8080:8080 javaapp:latest
 
