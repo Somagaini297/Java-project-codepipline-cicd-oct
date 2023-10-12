78d7d0d19da82640902fc123cb463da7745c18b0
S V Jagadeesh <vjagadeesh083@gmail.com>
2023-10-12 14:19:01 +0530

    upload appsec file

--- scripts/download_warfile.sh
+++ scripts/download_warfile.sh
@@ -5,6 +5,6 @@ cd /opt
 
 sleep 60
 
-aws s3 cp s3://sample-java-app-awscodepipeline-cicd-654123/javaapp-oct/target/login##1.0.war .
+aws s3 cp s3://codepipeline-us-east-1-849518956158/javaapp-oct/target/login##1.0.war .
 
 
