aede7d025c44e72034504314a71ec0f249e37db8
S V Jagadeesh <vjagadeesh083@gmail.com>
2023-10-23 17:07:44 +0530

    update appsec file

--- scripts/run_docker_commands.sh
+++ scripts/run_docker_commands.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-docker run -d --name javaapp-container -p 8080:8080 649787745281.dkr.ecr.us-east-1.amazonaws.com/argocdimage:latest
+docker run -d --name javaapp-container -p 8080:8080 649787745281.dkr.ecr.us-east-1.amazonaws.com/javaapp:latest
 
 docker ps -a
