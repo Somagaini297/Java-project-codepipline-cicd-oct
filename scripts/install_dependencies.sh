c24158bb4a20fae9c0f2c30aab709d9ab15da032
S V Jagadeesh <vjagadeesh083@gmail.com>
2023-10-23 10:54:14 +0530

    update all files

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,14 +2,11 @@
 
 set -e
 
-amazon-linux-extras install docker=latest -y
+amazon-linux-extras install tomcat8.5=latest -y
 
-systemctl start docker
+amazon-linux-extras install java-openjdk11 -y
 
-systemctl status docker
 
-chmod 777 /var/run/docker.sock
 
-cd /opt
 
-aws s3 cp s3://sample-java-app-awscodepipeline-cicd-654123/javaapp/login##1.0.war .
+
