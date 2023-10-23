c4446e2dd5ecf6feac78891fd87a362cf10a2d78
Somagaini297 <93876306+Somagaini297@users.noreply.github.com>
2023-10-23 21:49:01 +0530

    Update run_docker_commands.sh

--- scripts/run_docker_commands.sh
+++ scripts/run_docker_commands.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 649787745281.dkr.ecr.us-east-1.amazonaws.com
+
 docker run -d --name javaapp-container -p 8080:8080 649787745281.dkr.ecr.us-east-1.amazonaws.com/javaapp:latest
 
 docker ps -a
